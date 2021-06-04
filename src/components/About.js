e470352bf7f8884ca8cadc42b712d3f62d557bb1
Waseem Anwar <waseemanwargiccl@gmail.com>
2021-06-04 14:52:53 +0500

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="constainer">
+            <h4 className="center">About</h4>
+            <p>lorem ipsum</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
